feceb617d58112f8d5a1e0e6a129a8db5d950586
Prajakta Nilesh Wani <pkwani12@gmail.com>
2022-02-02 22:33:18 +0530

    first commit

---
+++ src/Components/Covidapi.js
@@ -0,0 +1,61 @@
+import * as React from 'react';
+import { useEffect, useState } from 'react';
+
+const Covidapi = () => {
+
+    const [data, setData] = useState([]);
+
+    const getCovidData = async () => {
+        const res = await fetch('https://data.covid19india.org/data.json');
+        const actualData = await res.json();
+        setData(actualData.statewise);
+    }
+
+    useEffect(() => {
+        getCovidData();
+    }, []);
+
+    return (
+        <div className='container'>
+            <h1>India Covid 19 Dashboard</h1>
+
+            <table className='table table-striped table-hover'>
+                <thead>
+                    <tr>
+                        <th>State</th>
+                        <th>Confirmed</th>
+                        <th>Recovered</th>
+                        <th>Deaths</th>
+                        <th>Active</th>
+                        <th>Updates</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        data.map((curElem, ind) => {
+                            return (
+                                <tr>
+                                    <td>{curElem.state}</td>
+                                    <td>{curElem.confirmed}</td>
+                                    <td>{curElem.recovered}</td>
+                                    <td>{curElem.deaths}</td>
+                                    <td>{curElem.actives}</td>
+                                    <td>{curElem.lastupdatedtime}</td>
+                                </tr>
+                            );
+                        })
+                    }
+                </tbody>
+            </table>
+
+
+
+
+
+        </div>
+    );
+
+}
+
+
+export default Covidapi;
